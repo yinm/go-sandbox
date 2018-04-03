5e3ee9425c168ffe8a8f79b93826b49402161e2b
yinm <yinm.cminor9@gmail.com>
2018-04-03 09:13:46 +0900

    Check fmt type Stringer

---
+++ fmt_type_stringer.go
@@ -0,0 +1,25 @@
+// https://golang.org/pkg/fmt/#Stringer
+package main
+
+import (
+	"fmt"
+)
+
+// Animal has a Name and an Age to represent an animal.
+type Animal struct {
+	Name string
+	Age uint
+}
+
+// String makes Animal satisfy the Stringer interface.
+func (a Animal) String() string {
+	return fmt.Sprintf("%v (%d)", a.Name, a.Age)
+}
+
+func main() {
+	a := Animal{
+		Name: "Gopher",
+		Age: 2,
+	}
+	fmt.Println(a)
+}
